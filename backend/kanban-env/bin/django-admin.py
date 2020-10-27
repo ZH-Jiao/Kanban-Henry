5fd54b816bf4fc50ee56f41f8380500d1b2d46df
Zhiheng Jiao <zhihengj@andrew.cmu.edu>
2020-10-26 20:04:08 -0400

    Initiate backend. Modify file structure

---
+++ backend/kanban-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jzh/Onedrive/计算机/Collev_project/kanban-henry/backend/kanban-env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
